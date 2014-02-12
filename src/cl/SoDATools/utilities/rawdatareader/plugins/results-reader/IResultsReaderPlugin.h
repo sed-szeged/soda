9de940429e760db20faefcc2e8934feb853ef399
dtengeri <dtengeri@inf.u-szeged.hu>
2014-02-12 12:46:08 +0100

    Added LICENSE headers.

--- src/cl/SoDATools/utilities/rawdatareader/plugins/results-reader/IResultsReaderPlugin.h
+++ src/cl/SoDATools/utilities/rawdatareader/plugins/results-reader/IResultsReaderPlugin.h
@@ -1,3 +1,24 @@
+/*
+ * Copyright (C): 2013-2014 Department of Software Engineering, University of Szeged
+ *
+ * Authors: David Tengeri <dtengeri@inf.u-szeged.hu>
+ *
+ * This file is part of SoDA.
+ *
+ *  SoDA is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU Lesser General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  SoDA is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU Lesser General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Lesser General Public License
+ *  along with SoDA.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 #ifndef IRESULTSREADERPLUGIN_H
 #define IRESULTSREADERPLUGIN_H
 
