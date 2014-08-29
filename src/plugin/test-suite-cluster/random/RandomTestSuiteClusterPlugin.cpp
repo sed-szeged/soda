d7dbebfaa576b916227323d3e1e345642b210712
dtengeri <dtengeri@inf.u-szeged.hu>
2014-08-29 16:47:57 +0200

    Added random test suite cluster algorithm. It creates cluster with the given sizes. The size is given in the percentage of the number of test cases. Eg: 10, 25, etc.

---
+++ src/plugin/test-suite-cluster/random/RandomTestSuiteClusterPlugin.cpp
@@ -0,0 +1,82 @@
+/*
+ * Copyright (C): 2013-2014 Department of Software Engineering, University of Szeged
+ *
+ * Authors: David Tengeri <dtengeri@inf.u-szeged.hu>
+ *
+ * This file is part of SoDA.
+ *
+ *  SoDA is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU Lesser General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  SoDA is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU Lesser General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Lesser General Public License
+ *  along with SoDA.  If not, see <http://www.gnu.org/licenses/>.
+ */
+#include <cmath>
+#include <fstream>
+
+#include "boost/lexical_cast.hpp"
+
+#include "RandomTestSuiteClusterPlugin.h"
+
+namespace soda {
+
+RandomTestSuiteClusterPlugin::RandomTestSuiteClusterPlugin() :
+    m_sizes(std::vector<IndexType>())
+{
+}
+
+RandomTestSuiteClusterPlugin::~RandomTestSuiteClusterPlugin()
+{
+}
+
+std::string RandomTestSuiteClusterPlugin::getName()
+{
+    return "random";
+}
+
+std::string RandomTestSuiteClusterPlugin::getDescription()
+{
+    return "Generates random test groups with different sizes given in the percentage of test cases.";
+}
+
+void RandomTestSuiteClusterPlugin::init(rapidjson::Document &doc)
+{
+    m_sizes.clear();
+    for (rapidjson::Value::ConstValueIterator it = doc["cluster-sizes"].Begin(); it != doc["cluster-sizes"].End(); it++) {
+        m_sizes.push_back(it->GetInt());
+    }
+}
+
+void RandomTestSuiteClusterPlugin::execute(CSelectionData &data, std::map<std::string, CClusterDefinition>& clusterList)
+{
+    std::vector<IndexType> testCaseIds(data.getCoverage()->getTestcases().getIDList());
+    for (std::vector<IndexType>::iterator it = m_sizes.begin(); it != m_sizes.end(); it++) {
+        IndexType size = std::floor(testCaseIds.size() * ((double)*it / 100));
+        if (size > testCaseIds.size()) {
+            // TODO Handle this error.
+            std::cerr << "Invalid size." << std::endl;
+            continue;
+        }
+        std::random_shuffle(testCaseIds.begin(), testCaseIds.end());
+        std::string clusterName = boost::lexical_cast<std::string>(*it);
+        for (IndexType i = 0; i < size; i++) {
+            clusterList[clusterName].addTestCase(testCaseIds[i]);
+        }
+
+        clusterList[clusterName].addCodeElements(data.getCoverage()->getCodeElements().getIDList());
+    }
+}
+
+extern "C" void registerPlugin(CKernel &kernel)
+{
+    kernel.getTestSuiteClusterPluginManager().addPlugin(new RandomTestSuiteClusterPlugin());
+}
+
+} /* namespace soda */
---
+++ src/plugin/test-suite-cluster/random/RandomTestSuiteClusterPlugin.h
@@ -0,0 +1,51 @@
+/*
+ * Copyright (C): 2013-2014 Department of Software Engineering, University of Szeged
+ *
+ * Authors: David Tengeri <dtengeri@inf.u-szeged.hu>
+ *
+ * This file is part of SoDA.
+ *
+ *  SoDA is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU Lesser General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  SoDA is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU Lesser General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Lesser General Public License
+ *  along with SoDA.  If not, see <http://www.gnu.org/licenses/>.
+ */
+#ifndef RANDOMTESTSUITECLUSTERPLUGIN_H
+#define RANDOMTESTSUITECLUSTERPLUGIN_H
+
+#include <vector>
+
+#include "engine/CKernel.h"
+
+namespace soda {
+
+class RandomTestSuiteClusterPlugin : public ITestSuiteClusterPlugin
+{
+public:
+
+    RandomTestSuiteClusterPlugin();
+    ~RandomTestSuiteClusterPlugin();
+
+    std::string getName();
+
+    std::string getDescription();
+
+    void init(rapidjson::Document &doc);
+
+    void execute(CSelectionData &data, std::map<std::string, CClusterDefinition>& clusterList);
+
+private:
+    std::vector<IndexType> m_sizes;
+};
+
+} /* namespace soda */
+
+#endif /* RANDOMTESTSUITECLUSTERPLUGIN_H */
